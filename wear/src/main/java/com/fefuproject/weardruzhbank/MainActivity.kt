a76942a8fe980e4311fc684f77837cbb1560f102
mikairyuu <miva200267@gmail.com>
2021-12-12 22:48:39 +1000

    wear: initial module structure

---
+++ wear/src/main/java/com/fefuproject/weardruzhbank/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.fefuproject.weardruzhbank
+
+import android.app.Activity
+import android.os.Bundle
+import com.fefuproject.weardruzhbank.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
