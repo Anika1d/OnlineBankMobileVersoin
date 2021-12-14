cbdd38016c55537b8d5aaa1c1345cda6ca4c3694
mikairyuu <miva200267@gmail.com>
2021-12-14 12:04:14 +1000

    wear: PIN auth done

--- wear/src/main/java/com/fefuproject/weardruzhbank/UI/AccountStateActivity.kt
+++ wear/src/main/java/com/fefuproject/weardruzhbank/UI/AccountStateActivity.kt
@@ -2,35 +2,64 @@ package com.fefuproject.weardruzhbank.UI
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.text.style.TextAlign
 import androidx.wear.compose.material.ExperimentalWearMaterialApi
 import androidx.wear.compose.material.Text
 
 @ExperimentalWearMaterialApi
 class AccountStateActivity : ComponentActivity() {
+    private var verified: MutableState<Boolean>? = null
+    private lateinit var authLauncher: ActivityResultLauncher<Intent>
+    private var recentlyVerified = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val contract = ActivityResultContracts.StartActivityForResult()
+        authLauncher = registerForActivityResult(contract) { result ->
+            if (result.resultCode != RESULT_OK) {
+                Toast.makeText(this, "Не удалось войти...", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            verified?.value = true
+            recentlyVerified = true
+        }
         setContent {
             RootView()
         }
     }
 
-
-    override fun onStart() {
-        super.onStart()
-        val contract = ActivityResultContracts.StartActivityForResult()
-        contract.createIntent(this, Intent(this, PasswordGuardActivity::class.java))
-        val launcher = registerForActivityResult(contract) { result ->
-            println("у меня есть результат!")
+    override fun onResume() {
+        super.onResume()
+        if (!recentlyVerified) {
+            verified?.value = false
+            recentlyVerified = true
+            authLauncher.launch(Intent(this, PasswordGuardActivity::class.java))
         }
-        launcher.launch(Intent(this, PasswordGuardActivity::class.java))
+        recentlyVerified = false
     }
 
     @Composable
     fun RootView() {
-        Text(text = "я аккаунтстейт ")
+        verified = remember { mutableStateOf(false) }
+        if (verified!!.value) {
+            AccountView()
+        } else {
+            Text(text = "Ожидание верификации...", textAlign = TextAlign.Center)
+        }
+    }
+
+    @Composable
+    fun AccountView() {
+        Text(text = "Верификация пройдена", textAlign = TextAlign.Center)
     }
 }
--- wear/src/main/java/com/fefuproject/weardruzhbank/UI/PasswordGuardActivity.kt
+++ wear/src/main/java/com/fefuproject/weardruzhbank/UI/PasswordGuardActivity.kt
@@ -1,16 +1,27 @@
 package com.fefuproject.weardruzhbank.UI
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.ExperimentalWearMaterialApi
+import androidx.wear.compose.material.Text
 
 
 @ExperimentalWearMaterialApi
@@ -27,13 +38,36 @@ class PasswordGuardActivity : ComponentActivity() {
     fun RootView() {
         var input by remember { mutableStateOf("") }
         val focusRequester = remember { FocusRequester() }
-        BasicTextField(
-            modifier = Modifier.focusRequester(focusRequester),
-            value = input,
-            onValueChange = { input = it },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
-            singleLine = true
-        )
+        Column(
+            Modifier
+                .padding(0.dp, 40.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Введите пин-код",
+                style = TextStyle(color = Color.White, textAlign = TextAlign.Center)
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            BasicTextField(
+                modifier = Modifier
+                    .focusRequester(focusRequester)
+                    .size(50.dp, 50.dp),
+                value = input,
+                onValueChange = { input = it },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.NumberPassword,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(onDone = {
+                    setResult(RESULT_OK) //TODO:Verify
+                    finish()
+                }),
+                singleLine = true,
+                textStyle = TextStyle(color = Color.White),
+                cursorBrush = SolidColor(Color.White)
+            )
+        }
         DisposableEffect(Unit) {
             focusRequester.requestFocus()
             onDispose { }
