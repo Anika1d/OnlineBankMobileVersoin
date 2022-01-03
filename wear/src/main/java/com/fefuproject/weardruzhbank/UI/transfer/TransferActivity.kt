006e4ad598dab10626c3fa7b4e626c6d46e8480c
mikairyuu <miva200267@gmail.com>
2022-01-03 23:29:58 +1000

    wear: transfer: basic layout finished

--- wear/src/main/java/com/fefuproject/weardruzhbank/UI/transfer/TransferActivity.kt
+++ wear/src/main/java/com/fefuproject/weardruzhbank/UI/transfer/TransferActivity.kt
@@ -9,16 +9,20 @@ import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.wear.compose.material.*
 import com.fefuproject.weardruzhbank.di.AuthStateObserver
 import com.fefuproject.weardruzhbank.R
 import com.fefuproject.weardruzhbank.extensions.DefaultScaffold
+import com.fefuproject.weardruzhbank.extensions.roundedPlaceholder
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -44,19 +48,20 @@ class TransferActivity : ComponentActivity() {
         val targetToolListState = rememberScalingLazyListState()
         val selectionError =
             sourceToolListState.layoutInfo.centralItemIndex == targetToolListState.layoutInfo.centralItemIndex
+                    || (sourceToolListState.layoutInfo.centralItemIndex == sourceToolListState.layoutInfo.totalItemsCount - 2)
+                    || (targetToolListState.layoutInfo.centralItemIndex == targetToolListState.layoutInfo.totalItemsCount - 2)
         Box(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(bottom = 5.dp), contentAlignment = Alignment.Center
         ) {
             Column(
-                modifier = Modifier.fillMaxHeight(),
+                modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
                 Spacer(modifier = Modifier.height(25.dp))
                 Text("Выберите счета", style = TextStyle(fontWeight = FontWeight.Light))
-
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -64,12 +69,20 @@ class TransferActivity : ComponentActivity() {
                     horizontalArrangement = Arrangement.Center,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    ToolSelector(listState = sourceToolListState, isError = selectionError)
+                    ToolSelector(
+                        listState = sourceToolListState,
+                        isError = selectionError,
+                        isLeft = true
+                    )
                     Image(
                         painter = painterResource(id = R.drawable.ic_baseline_arrow_forward_24),
                         contentDescription = "arrow"
                     )
-                    ToolSelector(listState = targetToolListState, isError = selectionError)
+                    ReverseToolSelector(
+                        listState = targetToolListState,
+                        isError = selectionError,
+                        isLeft = false
+                    )
                 }
                 if (isLoading) {
                     CircularProgressIndicator()
@@ -90,28 +103,135 @@ class TransferActivity : ComponentActivity() {
     }
 
     @Composable
-    fun ToolSelector(listState: ScalingLazyListState, isError: Boolean) {
+    fun ToolSelector(
+        listState: ScalingLazyListState,
+        isError: Boolean,
+        isLeft: Boolean,
+        viewModel: TransferViewModel = hiltViewModel()
+    ) {
+        val cards by viewModel.sourceCards.collectAsState()
+        val isLoading by viewModel.isLoading.collectAsState()
         ScalingLazyColumn(
             state = listState,
-            modifier = Modifier.width(50.dp)
+            modifier = Modifier.width(100.dp)
         ) {
             item {
-                Spacer(modifier = Modifier.size(25.dp))
+                Spacer(modifier = Modifier.height(25.dp))
             }
-            items(20) {
-                Text(
-                    "Тест",
-                    style = TextStyle(
-                        fontWeight =
-                        if (listState.layoutInfo.centralItemIndex - 1 == it)
-                            FontWeight.ExtraBold else FontWeight.Normal,
-                        color = if (it == listState.layoutInfo.centralItemIndex - 1 && isError)
-                            Color.Red else Color.White
-                    )
-                ) //TODO
+            items(cards?.size ?: 3) {
+                Row(
+                    horizontalArrangement = if (isLeft) Arrangement.End else Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    if (isLoading) {
+                        Text(
+                            "Тест",
+                            modifier = Modifier
+                                .roundedPlaceholder(true)
+                        )
+                    } else {
+                        Text(
+                            cards!!.get(it).instrumentType!!,
+                            style = TextStyle(
+                                fontWeight =
+                                if (listState.layoutInfo.centralItemIndex - 1 == it)
+                                    FontWeight.Normal else FontWeight.Light,
+                                color = if (it == listState.layoutInfo.centralItemIndex - 1 && isError)
+                                    Color.Red else Color.White,
+                            )
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(10.dp))
+                    if (isLoading) {
+                        Text(
+                            "Тест",
+                            modifier = Modifier
+                                .roundedPlaceholder(true)
+                        )
+                    } else {
+                        Text(
+                            cards!!.get(it).name ?: "*" + cards!!.get(it).number!!.drop(12),
+                            style = TextStyle(
+                                fontWeight =
+                                if (listState.layoutInfo.centralItemIndex - 1 == it)
+                                    FontWeight.ExtraBold else FontWeight.Normal,
+                                color = if (it == listState.layoutInfo.centralItemIndex - 1 && isError)
+                                    Color.Red else Color.White
+                            )
+                        )
+                    }
+                }
             }
             item {
-                Spacer(modifier = Modifier.size(25.dp))
+                Spacer(modifier = Modifier.height(25.dp))
+            }
+        }
+    }
+
+    @Composable
+    fun ReverseToolSelector( //THX GOOGLE FOR WORKING ZINDEX YEAH THAT'S SO GOOD THXXXXXXXXXX
+        listState: ScalingLazyListState,
+        isError: Boolean,
+        isLeft: Boolean,
+        viewModel: TransferViewModel = hiltViewModel()
+    ) {
+        val cards by viewModel.sourceCards.collectAsState()
+        val isLoading by viewModel.isLoading.collectAsState()
+        ScalingLazyColumn(
+            state = listState,
+            modifier = Modifier.width(100.dp)
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(25.dp))
+            }
+            items(cards?.size ?: 3) {
+                Row(
+                    horizontalArrangement = if (isLeft) Arrangement.End else Arrangement.Start,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    if (isLoading) {
+                        Text(
+                            "Тест",
+                            modifier = Modifier
+                                .roundedPlaceholder(true)
+                        )
+                    } else {
+                        Text(
+                            if (cards!!.get(it).name.isNullOrBlank()) "*" + cards!!.get(it).number!!.drop(
+                                12
+                            ) else cards!!.get(it).name!!,
+                            style = TextStyle(
+                                fontWeight =
+                                if (listState.layoutInfo.centralItemIndex - 1 == it)
+                                    FontWeight.ExtraBold else FontWeight.Normal,
+                                color = if (it == listState.layoutInfo.centralItemIndex - 1 && isError)
+                                    Color.Red else Color.White
+                            )
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(10.dp))
+                    if (isLoading) {
+                        Text(
+                            "Тест",
+                            modifier = Modifier
+                                .roundedPlaceholder(true)
+                        )
+                    } else {
+                        Text(
+                            cards!!.get(it).instrumentType!!,
+                            style = TextStyle(
+                                fontWeight =
+                                if (listState.layoutInfo.centralItemIndex - 1 == it)
+                                    FontWeight.Normal else FontWeight.Light,
+                                color = if (it == listState.layoutInfo.centralItemIndex - 1 && isError)
+                                    Color.Red else Color.White,
+                            )
+                        )
+                    }
+                }
+            }
+            item {
+                Spacer(modifier = Modifier.height(25.dp))
             }
         }
     }
--- wear/src/main/java/com/fefuproject/weardruzhbank/UI/transfer/TransferViewModel.kt
+++ wear/src/main/java/com/fefuproject/weardruzhbank/UI/transfer/TransferViewModel.kt
@@ -1,17 +1,33 @@
 package com.fefuproject.weardruzhbank.UI.transfer
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fefuproject.shared.account.domain.models.InstrumentModel
+import com.fefuproject.shared.account.domain.usecase.GetAllInstrumentsUseCase
+import com.fefuproject.weardruzhbank.di.PreferenceProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TransferViewModel @Inject constructor() : ViewModel() {
-    private var _isLoading = MutableStateFlow(false)
+class TransferViewModel @Inject constructor(
+    preferenceProvider: PreferenceProvider,
+    getAllInstrumentsUseCase: GetAllInstrumentsUseCase
+) : ViewModel() {
+    private var _isLoading = MutableStateFlow(true)
     val isLoading get() = _isLoading.asStateFlow()
+    private var _sourceCards = MutableStateFlow<List<InstrumentModel>?>(null)
+    val sourceCards get() = _sourceCards.asStateFlow()
 
-    fun startTransaction(){
-        _isLoading.value = true;
+    init {
+        viewModelScope.launch {
+            _sourceCards.value = getAllInstrumentsUseCase.invoke(preferenceProvider.token!!)
+            _isLoading.value = false
+        }
+    }
+
+    fun startTransaction() {
     }
 }
