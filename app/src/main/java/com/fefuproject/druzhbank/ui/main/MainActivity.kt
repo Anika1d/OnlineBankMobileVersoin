854dd8929f5152ef958b1423bc63a9c431e07e10
mikairyuu <miva200267@gmail.com>
2022-01-03 19:57:15 +1000

    app: main: add auth and wear data transmission

--- app/src/main/java/com/fefuproject/druzhbank/ui/main/MainActivity.kt
+++ app/src/main/java/com/fefuproject/druzhbank/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.fefuproject.druzhbank
+package com.fefuproject.druzhbank.ui.main
 
 import android.content.Context
 import android.content.Intent
@@ -6,28 +6,36 @@ import android.os.Bundle
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.fefuproject.druzhbank.BankActivity
+import com.fefuproject.druzhbank.ProfileActivity
+import com.fefuproject.druzhbank.R
+import com.fefuproject.druzhbank.ValuteActivity
 import com.fefuproject.druzhbank.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.includeButtonBunk.conBank.setOnClickListener{
+        binding.includeButtonBunk.conBank.setOnClickListener {
             val intentt = Intent(this, BankActivity::class.java) //переход на другую активити
             startActivity(intentt)
         }
-        binding.includeButtonValute.conValute.setOnClickListener{
-
+        binding.includeButtonValute.conValute.setOnClickListener {
             val intentt = Intent(this, ValuteActivity::class.java)
             startActivity(intentt)
         }
@@ -37,9 +45,9 @@ class MainActivity : AppCompatActivity() {
         val currentDate = Date() ///заполнение изменяемых данных
         val dateFormat: DateFormat = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
         val dateText = dateFormat.format(currentDate)
-        binding.includeButtonValute.date.text=dateText.toString()
-        binding.includeButtonValute.eurText.text="EUR 166,5" ///ТАКЖЕ ИЗ БД
-        binding.includeButtonValute.usdText.text="USD 70,1"
+        binding.includeButtonValute.date.text = dateText.toString()
+        binding.includeButtonValute.eurText.text = "EUR 166,5" ///ТАКЖЕ ИЗ БД
+        binding.includeButtonValute.usdText.text = "USD 70,1"
         setContentView(binding.root)
     }
 
@@ -51,20 +59,23 @@ class MainActivity : AppCompatActivity() {
             LayoutInflater.from(this).inflate(R.layout.dialog_authorize, null)
         alert.setView(promptsView)
         alert.setCancelable(false)
-        val bun_cancel=promptsView.findViewById<MaterialButton>(R.id.cancel_button)
-        bun_cancel.setOnClickListener{
-           alert.dismiss()
+        val bun_cancel = promptsView.findViewById<MaterialButton>(R.id.cancel_button)
+        bun_cancel.setOnClickListener {
+            alert.dismiss()
         }
-        val bun_auth=promptsView.findViewById<MaterialButton>(R.id.auth_button)
-        bun_auth.setOnClickListener{
-           val intentt = Intent(this, ProfileActivity::class.java)
-            startActivity(intentt)
+        val bun_auth = promptsView.findViewById<MaterialButton>(R.id.auth_button)
+        bun_auth.setOnClickListener {
+            viewModel.login(
+                promptsView.findViewById<TextInputEditText>(R.id.login_edit).text.toString(),
+                promptsView.findViewById<TextInputEditText>(R.id.password_edit).text.toString(),
+                this
+            ) { startActivity(Intent(this, ProfileActivity::class.java)) }
         }
 
         alert.show()
     }
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
 
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
         return super.onCreateView(name, context, attrs)
-        }
+    }
 }
---
+++ app/src/main/java/com/fefuproject/druzhbank/ui/main/MainActivityViewModel.kt
@@ -0,0 +1,41 @@
+package com.fefuproject.druzhbank.ui.main
+
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fefuproject.druzhbank.di.PreferenceProvider
+import com.fefuproject.shared.account.domain.usecase.LogInUseCase
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.PutDataRequest
+import com.google.android.gms.wearable.Wearable
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(
+    private val loginUserUseCase: LogInUseCase,
+    private val preferenceProvider: PreferenceProvider
+) : ViewModel() {
+
+    fun login(username: String, password: String, context: Context, onSuccessListener: () -> Unit) {
+        viewModelScope.launch {
+            val user = loginUserUseCase.invoke(username, password)
+            if (user != null) {
+                preferenceProvider.updateToken(user.token!!)
+                val request = PutDataRequest.create("/token").apply {
+                    setUrgent()
+                    data = user.token!!.toByteArray()
+                }
+                Wearable.getDataClient(context).putDataItem(request)
+                onSuccessListener.invoke()
+            } else {
+                Toast.makeText(context, "Вход не удался...", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
