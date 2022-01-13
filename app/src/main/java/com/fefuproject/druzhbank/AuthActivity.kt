a565173fdcba23c862472a508a0fd19806aefc60
mikairyuu <miva200267@gmail.com>
2022-01-14 01:36:47 +1000

    app: auth: add API name and fix minor issues

--- app/src/main/java/com/fefuproject/druzhbank/AuthActivity.kt
+++ app/src/main/java/com/fefuproject/druzhbank/AuthActivity.kt
@@ -39,7 +39,11 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import com.fefuproject.druzhbank.ui.theme.DruzhbankTheme
 import com.fefuproject.druzhbank.di.PreferenceProvider
+import com.fefuproject.shared.account.domain.usecase.GetUserUseCase
+import com.google.android.gms.wearable.PutDataRequest
+import com.google.android.gms.wearable.Wearable
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -48,11 +52,25 @@ class AuthActivity : FragmentActivity() {
     @Inject
     lateinit var preferenceProvider: PreferenceProvider
 
+    @Inject
+    lateinit var getUserUseCase: GetUserUseCase
+    private val authLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            launchBiometrics(
+                true
+            )
+        }
+
+    private var username: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (preferenceProvider.biometricPreference == PreferenceProvider.STATUS_BIOMETRICS_ENABLED) launchBiometrics(
             false
         )
+        runBlocking {
+            username = getUserUseCase.invoke(preferenceProvider.token!!)?.name
+        }
         setContent {
             DruzhbankTheme {
                 Surface(color = MaterialTheme.colors.background) {
@@ -106,8 +124,6 @@ class AuthActivity : FragmentActivity() {
                 biometricPrompt.authenticate(promptInfo)
             }
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
-                val authLauncher =
-                    registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {}
                 authLauncher.launch(Intent(Settings.ACTION_FINGERPRINT_ENROLL).apply {
                     putExtra(
                         "android.provider.extra.BIOMETRIC_AUTHENTICATORS_ALLOWED",
@@ -123,13 +139,14 @@ class AuthActivity : FragmentActivity() {
         }
     }
 
-
     @Composable
     fun RootView(firstTime: Boolean = false) {
         var input by remember { mutableStateOf("") }
         if (input.length == 5) {
             if (firstTime) {
                 preferenceProvider.updatePrivateKey(input)
+                Wearable.getDataClient(this)
+                    .putDataItem(PutDataRequest.create("/pin").apply { data = input.toByteArray() })
                 launchBiometrics(true)
             } else if (input != preferenceProvider.privateKey) {
                 input = ""
@@ -151,14 +168,16 @@ class AuthActivity : FragmentActivity() {
             verticalArrangement = Arrangement.spacedBy(50.dp)
         ) {
             Image(
-                modifier = Modifier.scale(3f).offset(0.dp, 23.dp),
+                modifier = Modifier
+                    .scale(3f)
+                    .offset(0.dp, 23.dp),
                 painter = painterResource(R.drawable.logo),
                 contentDescription = "background",
                 contentScale = ContentScale.FillBounds
             )
             Spacer(modifier = Modifier.size(50.dp))
             Text(
-                text = "Здравствуйте, <Имя>",
+                text = "Здравствуйте, " + username,
                 style = TextStyle(
                     color = Color.Black,
                     fontWeight = FontWeight.Bold,
