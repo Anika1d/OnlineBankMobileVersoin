98542e08b16316257cd28a6aaf2659e95685100f
mikairyuu <miva200267@gmail.com>
2022-01-14 02:42:45 +1000

    wear: payment: make demo payment layout

--- wear/src/main/java/com/fefuproject/druzhbank/UI/payment/PaymentActivity.kt
+++ wear/src/main/java/com/fefuproject/druzhbank/UI/payment/PaymentActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.wear.compose.material.ExperimentalWearMaterialApi
-import androidx.wear.compose.material.Text
+import androidx.wear.compose.material.*
+import com.fefuproject.druzhbank.extensions.DefaultScaffold
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.google.android.gms.wearable.DataClient
 import com.google.android.gms.wearable.DataEventBuffer
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,19 +35,62 @@ class PaymentActivity : ComponentActivity(), DataClient.OnDataChangedListener {
     @ExperimentalWearMaterialApi
     @Composable
     fun RootView(viewModel: PaymentActivityViewModel = hiltViewModel()) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Не удалось получить ни одного шаблона...",
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = "Создайте несколько в приложении",
-                textAlign = TextAlign.Center
-            )
+        val scalingLazyListState = rememberScalingLazyListState()
+        DefaultScaffold(scalingLazyListState = scalingLazyListState) {
+            if (viewModel.payments == null) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "Не удалось получить ни одного шаблона...",
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        text = "Создайте несколько в приложении",
+                        textAlign = TextAlign.Center
+                    )
+                }
+            } else {
+                val isRefreshing by viewModel.isRefreshing.collectAsState()
+                SwipeRefresh(
+                    state = rememberSwipeRefreshState(isRefreshing),
+                    swipeEnabled = false,
+                    onRefresh = {},
+                ) {
+                    ScalingLazyColumn(
+                        state = scalingLazyListState,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        contentPadding = PaddingValues(
+                            top = 30.dp,
+                            bottom = 30.dp,
+                            start = 50.dp,
+                            end = 50.dp
+                        ),
+                        modifier = Modifier.fillMaxHeight(),
+                        scalingParams = ScalingLazyColumnDefaults.scalingParams(
+                            minTransitionArea = 1f,
+                            maxTransitionArea = 1f
+                        )
+                    ) {
+                        items(viewModel.payments) {
+                            Button(
+                                modifier = Modifier.size(120.dp),
+                                onClick = { viewModel.makePayment(it, applicationContext) }) {
+                                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                                    Text(text = it.name, textAlign = TextAlign.Center)
+                                    Spacer(modifier = Modifier.height(7.dp))
+                                    Text(
+                                        text = it.amount.toString() + "р",
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
--- wear/src/main/java/com/fefuproject/druzhbank/UI/payment/PaymentActivityViewModel.kt
+++ wear/src/main/java/com/fefuproject/druzhbank/UI/payment/PaymentActivityViewModel.kt
@@ -1,17 +1,58 @@
 package com.fefuproject.druzhbank.UI.payment
 
+import android.content.Context
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.fefuproject.druzhbank.di.PreferenceProvider
+import com.fefuproject.shared.account.domain.enums.PayType
 import com.fefuproject.shared.account.domain.usecase.PayCategoryUseCase
+import com.fefuproject.shared.account.domain.usecase.PayUniversalUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class PaymentActivityViewModel @Inject constructor(
+    private val preferenceProvider: PreferenceProvider,
+    private val payUniversalUseCase: PayUniversalUseCase
 ) : ViewModel() {
+    private var _isRefreshing = MutableStateFlow(false)
+    val isRefreshing get() = _isRefreshing.asStateFlow()
 
-    init {
-        
+    val payments: List<PaymentModel>? = listOf(
+        PaymentModel("Общага", 3990, "5238393521747493", PayType.onCard.ordinal, "ДВФУ"),
+        PaymentModel("ЖКХ", 7000, "5238393521747493", PayType.onCard.ordinal, "ЖКХ"),
+        PaymentModel("Связь", 200, "5238393521747493", PayType.onCard.ordinal, "Связь")
+    )
+
+    fun makePayment(payment: PaymentModel, context: Context) {
+        viewModelScope.launch {
+            _isRefreshing.value = true
+            val res = payUniversalUseCase.invoke(
+                payment.source,
+                payment.id.toString(),
+                payment.amount.toDouble(),
+                payment.sourceType,
+                PayType.onCategory.ordinal,
+                preferenceProvider.token!!
+            )
+            Toast.makeText(
+                context,
+                if (res) "Платёж успешно совершён" else "При совершении платежа произошла ошибка...",
+                Toast.LENGTH_SHORT
+            ).show()
+            _isRefreshing.value = false
+        }
     }
 }
+
+data class PaymentModel(
+    val name: String,
+    val amount: Int,
+    val source: String,
+    val sourceType: Int,
+    val id: String,
+)
