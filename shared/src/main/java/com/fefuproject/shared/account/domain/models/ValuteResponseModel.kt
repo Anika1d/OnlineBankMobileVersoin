f4a4ea83e9f11eb8abc7e6a1072475ca95a3c0b5
mikairyuu <miva200267@gmail.com>
2022-01-13 17:20:03 +1000

    shared: valuteModel: adapt to API changes

--- shared/src/main/java/com/fefuproject/shared/account/domain/models/ValuteResponseModel.kt
+++ shared/src/main/java/com/fefuproject/shared/account/domain/models/ValuteResponseModel.kt
@@ -2,11 +2,15 @@ package com.fefuproject.shared.account.domain.models
 
 
 data class ValuteResponseModel(
-    val date: String,
-    val rates: List<ValuteModel>,
+    val ValCurs: ValCursModel,
+)
+
+data class ValCursModel(
+    val Valute: List<ValuteModel>
 )
 
 data class ValuteModel(
-    val currencyCode: String,
-    val currencyRate: Double,
+    val CharCode: String,
+    val Value: String,
+    val Name: String,
 )
