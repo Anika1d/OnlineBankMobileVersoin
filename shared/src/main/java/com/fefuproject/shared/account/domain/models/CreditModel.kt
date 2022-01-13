4ac09b3d9ff2c599704d4ad9dd831bc3e0037e93
mikairyuu <miva200267@gmail.com>
2022-01-14 00:05:50 +1000

    shared: creditModel: fix payment due

--- shared/src/main/java/com/fefuproject/shared/account/domain/models/CreditModel.kt
+++ shared/src/main/java/com/fefuproject/shared/account/domain/models/CreditModel.kt
@@ -1,10 +1,12 @@
 package com.fefuproject.shared.account.domain.models
 
+import java.util.*
+
 data class CreditModel(
     val id: Int,
     val user_id: Int?,
     val name: String,
     val number: String,
     val count: String,
-    val payment_date: String,
+    val payment_date: Date,
 )
