5d059d63f17e9789ff75207f60909f47cf8afd83
mikairyuu <miva200267@gmail.com>
2022-01-27 11:48:07 +0300

    wear: extensions: fix partial lists merging

--- wear/src/main/java/com/fefuproject/druzhbank/extensions/extensions.kt
+++ wear/src/main/java/com/fefuproject/druzhbank/extensions/extensions.kt
@@ -118,13 +118,15 @@ fun <T> List<T?>.addNullList(n: Int): List<T?> {
 // always merges from end
 // OOBE-safe for the list being merged
 fun <T> List<T>.mergeFromList(another: List<T>, n: Int): List<T> {
-    val newList = this.toMutableList()
+    var newList = this.toMutableList()
     var outCounter = 0
     repeat(n) {
-        val i = it + 1
-        if (another.size - i >= 0) newList[size - i] = another[another.size - i]
+        val i = n - it
+        if (another.size > (n - i)) newList[size - i] = another[n - i]
         else outCounter++
     }
-    if (outCounter > 0) newList.dropLast(outCounter) // effectively merged nothing from the merged list this way
+    if (outCounter > 0)
+        newList = newList.dropLast(outCounter)
+            .toMutableList() // effectively merged nothing from the merged list this way
     return newList
 }
