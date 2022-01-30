51eef45d91643d326e3d57a59932d196e01feaf8
Anikald <roslyakov.vo@students.dvfu.ru>
2022-01-30 17:17:47 +1000

    app: pay connected to viewModel

--- app/src/main/java/com/fefuproject/druzhbank/dirprofile/pay/PayFragment.kt
+++ app/src/main/java/com/fefuproject/druzhbank/dirprofile/pay/PayFragment.kt
@@ -10,6 +10,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.viewModels
 import com.fefuproject.druzhbank.R
 import com.fefuproject.druzhbank.databinding.FragmentPayBinding
 import com.fefuproject.druzhbank.di.PreferenceProvider
@@ -26,45 +27,37 @@ import javax.inject.Inject
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 @AndroidEntryPoint
-class PayFragment(val checkModel: CheckModel) : Fragment() {
+class PayFragment() : Fragment() {
     private var _binding: FragmentPayBinding? = null
     private val binding get() = _binding!!
-
+    private val viewModel: PayViewModel by viewModels()
 
     @Inject
     lateinit var editInstrumentNameUseCase: EditInstrumentNameUseCase
+
     @Inject
     lateinit var preferenceProvider: PreferenceProvider
+
+    private lateinit var tmp_number_pay: String
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        tmp_number_pay = arguments?.getString("myArg").toString()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentPayBinding.inflate(inflater, container, false)
+        binding.shimmerFrameLayout.startShimmer()
+        viewModel.initData(tmp_number_pay, binding, this)
         return binding.root
     }
 
     @SuppressLint("FragmentBackPressedCallback", "SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.includePay.fullNumberPay.text = "*********" + checkModel.number.takeLast(4)
-        binding.includePay.valuePay.text = checkModel.count + " руб."
-        binding.includePay.namePay.text = checkModel.name
-        binding.historyCardButton.setOnClickListener {
-            parentFragmentManager.beginTransaction().apply {
-                val visibleFragment =
-                    parentFragmentManager.fragments.firstOrNull { !isHidden }
-                visibleFragment?.let {
-                    hide(visibleFragment)
-                }
-                replace(
-                    R.id.fragmentContainerViewProfile,
-                    HistoryPayFragment(checkModel), "PayHistory"
-                )
-                commit()
-            }
-        }
-
         activity?.onBackPressedDispatcher?.addCallback(
             this@PayFragment,
             object : OnBackPressedCallback(true) {
@@ -78,60 +71,9 @@ class PayFragment(val checkModel: CheckModel) : Fragment() {
                     }
                 }
             })
-        binding.renameCard.setOnClickListener {
-            AlertDialogRenamePay()
-        }
-
     }
 
-    private fun AlertDialogRenamePay() {
-        val builder = AlertDialog.Builder(this@PayFragment.requireActivity())
-        val alert = builder.create();
-        val promptsView: View =
-            LayoutInflater.from(this@PayFragment.requireActivity())
-                .inflate(R.layout.dialog_rename_card, null)
-        alert.setView(promptsView)
-        alert.setCancelable(false)
-        val bun_cancel = promptsView.findViewById<MaterialButton>(R.id.cancel_button_rename)
-        bun_cancel.setOnClickListener {
-            Toast.makeText(
-                this@PayFragment.requireActivity().applicationContext,
-                "Операция отменена",
-                Toast.LENGTH_SHORT
-            ).show()
-            alert.dismiss()
-        }
-        val newName = promptsView.findViewById<EditText>(R.id.name_card_edit_text)
-        val bun_bl_d = promptsView.findViewById<MaterialButton>(R.id.rename_button_card)
-        bun_bl_d.setOnClickListener {
-            if (newName.text.toString() != "") {
-                Toast.makeText(
-                    this@PayFragment.requireActivity().applicationContext,
-                    "Вы переименовали счёт",
-                    Toast.LENGTH_SHORT
-                ).show()
-                runBlocking {
-                    editInstrumentNameUseCase.invoke(
-                        preferenceProvider.token!!,
-                        newName.text.toString(),
-                        checkModel.number,
-                        InstrumetType.Check,
-                    )
-                }
-                binding.includePay.namePay.text = newName.text
-                alert.dismiss()
-            } else {
 
-                Toast.makeText(
-                    this@PayFragment.requireActivity().applicationContext,
-                    "Вы не ввели новое имя карты",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-        alert.show()
-
-    }
 
 
 }
---
+++ app/src/main/java/com/fefuproject/druzhbank/dirprofile/pay/PayViewModel.kt
@@ -0,0 +1,120 @@
+package com.fefuproject.druzhbank.dirprofile.pay
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.fefuproject.druzhbank.R
+import com.fefuproject.druzhbank.databinding.FragmentPayBinding
+import com.fefuproject.druzhbank.di.PreferenceProvider
+import com.fefuproject.druzhbank.dirprofile.fragmentprofile.ProfileMainFragment
+import com.fefuproject.druzhbank.dirprofile.pay.historypay.HistoryPayFragment
+import com.fefuproject.shared.account.domain.enums.InstrumetType
+import com.fefuproject.shared.account.domain.models.CheckModel
+import com.fefuproject.shared.account.domain.usecase.EditInstrumentNameUseCase
+import com.fefuproject.shared.account.domain.usecase.GetChecksByNumberUseCase
+import com.google.android.material.button.MaterialButton
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+
+@HiltViewModel
+class PayViewModel @Inject constructor(
+    private val preferenceProvider: PreferenceProvider,
+    private val getCheckUseCase: GetChecksByNumberUseCase,
+    private val editInstrumentNameUseCase: EditInstrumentNameUseCase
+) : ViewModel() {
+
+    private lateinit var checkModel: CheckModel
+    private lateinit var binding: FragmentPayBinding
+    private lateinit var payFragment: PayFragment
+
+
+    @SuppressLint("SetTextI18n")
+    fun initData(tmpNumberPay: String, _binding: FragmentPayBinding, _payFragment: PayFragment) {
+        binding = _binding
+        payFragment = _payFragment
+        viewModelScope.launch {
+
+            checkModel = getCheckUseCase(preferenceProvider.token!!, tmpNumberPay)!![0]
+            binding.includePay.fullNumberPay.text = "*********" + checkModel.number.takeLast(4)
+            binding.includePay.valuePay.text = checkModel.count + " руб."
+            binding.includePay.namePay.text = checkModel.name
+            binding.shimmerFrameLayout.stopShimmer()
+            binding.shimmerFrameLayout.visibility=View.GONE
+            binding.includePay.root.visibility=View.VISIBLE
+            binding.historyCardButton.visibility=View.VISIBLE
+            binding.renameCard.visibility=View.VISIBLE
+            binding.historyCardButton.setOnClickListener {
+                payFragment.parentFragmentManager.beginTransaction().apply {
+                    replace(
+                        R.id.fragmentContainerViewProfile,
+                        HistoryPayFragment(checkModel), "PayHistory"
+                    )
+                    commit()
+                }
+            }
+
+            binding.renameCard.setOnClickListener {
+                AlertDialogRenamePay()
+            }
+
+        }
+    }
+
+    private fun AlertDialogRenamePay() {
+        val builder = AlertDialog.Builder(payFragment.requireActivity())
+        val alert = builder.create();
+        val promptsView: View =
+            LayoutInflater.from(payFragment.requireActivity())
+                .inflate(R.layout.dialog_rename_card, null)
+        alert.setView(promptsView)
+        alert.setCancelable(false)
+        val bun_cancel = promptsView.findViewById<MaterialButton>(R.id.cancel_button_rename)
+        bun_cancel.setOnClickListener {
+            Toast.makeText(
+                payFragment.requireActivity().applicationContext,
+                "Операция отменена",
+                Toast.LENGTH_SHORT
+            ).show()
+            alert.dismiss()
+        }
+        val newName = promptsView.findViewById<EditText>(R.id.name_card_edit_text)
+        val bun_bl_d = promptsView.findViewById<MaterialButton>(R.id.rename_button_card)
+        bun_bl_d.setOnClickListener {
+            viewModelScope.launch {
+                if (newName.text.toString() != "") {
+                    Toast.makeText(
+                        payFragment.requireActivity().applicationContext,
+                        "Вы переименовали счёт",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    editInstrumentNameUseCase.invoke(
+                        preferenceProvider.token!!,
+                        newName.text.toString(),
+                        checkModel.number,
+                        InstrumetType.Check,
+                    )
+                    binding.includePay.namePay.text = newName.text
+                    alert.dismiss()
+                } else {
+
+                    Toast.makeText(
+                        payFragment.requireActivity().applicationContext,
+                        "Вы не ввели новое имя карты",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+        alert.show()
+
+    }
+}
